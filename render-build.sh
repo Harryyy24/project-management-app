5d1b56c60e05655d597229cf54a521c6043bb574
Harryyy24 <167174808+Harryyy24@users.noreply.github.com>
2025-05-28 16:52:34 +0530

    Simplify deployment with Procfile

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-# Set Java home and path
-export JAVA_HOME=/usr/lib/jvm/temurin-17-jdk-amd64
-export PATH=$JAVA_HOME/bin:$PATH
-
-# Verify Java installation
-java -version
+# Make mvnw executable
+chmod +x mvnw
 
 # Build the application
 ./mvnw clean package -DskipTests
--- render-start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Set Java home and path
-export JAVA_HOME=/usr/lib/jvm/temurin-17-jdk-amd64
-export PATH=$JAVA_HOME/bin:$PATH
-
-# Start the application
-java -jar backend/target/*.jar
