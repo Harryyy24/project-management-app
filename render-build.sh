878502b951ebbd6a928725418aaa8cae9b75dab7
Harryyy24 <167174808+Harryyy24@users.noreply.github.com>
2025-05-28 16:08:36 +0530

    Add Java 17 configuration and update build script

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,9 @@
-
 #!/bin/bash
 # Set execute permissions
 chmod +x backend/mvnw
+
+# Set Java home (for Render)
+export JAVA_HOME=/usr/lib/jvm/temurin-17-jdk-amd64
 
 # Run the Maven build
 cd backend && ./mvnw clean package
